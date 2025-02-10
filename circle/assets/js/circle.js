5917b9d78276057643e2fc92d735620a60d0e1c6
klmerritt44 <143474174+klmerritt44@users.noreply.github.com>
2025-02-10 00:05:42 +0000

    circle calculator calculate cirle working

--- circle/assets/js/circle.js
+++ circle/assets/js/circle.js
@@ -1,18 +1,39 @@
 
 "use strict";
     
-    
 $( "#CircleForm" ).validate({
 
 });
 
-function CircleCalculations() {
+function calculateCircle() {
     if ($("#CircleForm").valid()) {
-        let x = 1;
-    window.alert("Start of Calculations");
-}
+        let radius = document.getElementById("radius").value;
+        let radiusfp = parseFloat(radius);
+        let diameter = calculateDiameter(radiusfp);
+        document.getElementById("diameter").innerHTML = diameter;
+        let circumference = calculateCircumference(radiusfp);
+        document.getElementById("circumference").innerHTML = circumference;
+        let area = calculateArea(radiusfp);
+        document.getElementById("area").innerHTML = area;
+    }
 }
 
 function calculateDiameter(r) {
     return 2 * r;
+}
+
+function calculateCircumference(r) {
+    return 2 * Math.PI * r;
+}
+
+function calculateArea(r) {
+    return Math.PI * r * r;
+}
+
+function clearForm() {
+    document.getElementById("radius").value = "";
+    document.getElementById("radiuserror").innerHTML = "";
+    document.getElementById("diameter").innerHTML = "";
+    document.getElementById("circumference").innerHTML = "";
+    document.getElementById("area").innerHTML = "";
 }

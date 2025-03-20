6bef82fea784be35fb691c1d6e6ca682b0c2a42a
klmerritt44 <143474174+klmerritt44@users.noreply.github.com>
2025-03-20 01:28:23 +0000

    added site reflection and blog post link

--- mini-site/assets/js/script.js
+++ mini-site/assets/js/script.js
@@ -45,6 +45,38 @@ $(document).ready(function() {
 function getTemperature() {
    alert("Hello Friend!")
 }
+//contact form script//
+ /* paste this line in verbatim */
+ window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+ /* customize formbutton below*/     
+ formbutton("create", {
+   action: "https://formspree.io/f/xkgjnlwj",
+   title: "How can we help?",
+   fields: [
+     { 
+       type: "email", 
+       label: "Email:", 
+       name: "email",
+       required: true,
+       placeholder: "your@email.com"
+     },
+     {
+       type: "textarea",
+       label: "Message:",
+       name: "message",
+       placeholder: "What's on your mind?",
+     },
+     { type: "submit" }      
+   ],
+   styles: {
+     title: {
+       backgroundColor: "gray"
+     },
+     button: {
+       backgroundColor: "gray"
+     }
+   }
+ });
 
 
 //Nav bar Script Start//

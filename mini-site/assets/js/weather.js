75ff8ab41836e5b48f0f940ef7f92500b933978d
klmerritt44 <143474174+klmerritt44@users.noreply.github.com>
2025-03-23 23:36:44 +0000

    add blank error message and location error message to weather

--- mini-site/assets/js/weather.js
+++ mini-site/assets/js/weather.js
@@ -3,12 +3,20 @@
 
  function getTemperature() {
     let locationValue = document.getElementById("weather-location").value;
+    if (locationValue == "") {
+        alert("Please enter a city name.");
+        return;
+    }
+     
     locationValue = encodeURIComponent(locationValue);
     let weatherURL = "https://geocoding-api.open-meteo.com/v1/search?name=" + locationValue + "&count=10&language=en&format=json";
     fetch(weatherURL)
-    //fetch("https://geocoding-api.open-meteo.com/v1/search?name=Eureka+Springs&count=10&language=en&format=json")
     .then(response => response.json())
     .then(json => {
+        if (!json.results) {
+            alert("No results. Try a different city name.")
+            return;
+        }
         let latitude = json.results[0].latitude;
         let longitude = json.results[0].longitude;
         fetch("https://api.open-meteo.com/v1/forecast?latitude="+latitude+"&longitude="+longitude+"&hourly=temperature_2m&temperature_unit=fahrenheit&timezone=auto")
